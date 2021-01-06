a64ed25c12d108c5fb5820a1019d9fdb813c0e5b
junpu <junpu0311@163.com>
2021-01-06 22:04:48 +0800

    modify login

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,18 +1,17 @@
 const Result = require("../app/result");
-const jwt = require('jsonwebtoken');
 const { sign } = require("../app/token");
 
 class AuthController {
     async login(ctx, next) {
         console.log("AuthController被调用");
-        const { id, name } = ctx.user;
+        const { id, name, avatar } = ctx.user;
         let token = null;
         try {
             token = sign({ id, name });
         } catch (error) {
             ctx.app.emit("error", error, ctx);
         }
-        const result = { id, name, token };
+        const result = { id, name, avatar, token };
         ctx.body = new Result(null, "用户登录成功", result);;
     }
 }
